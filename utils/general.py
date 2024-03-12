4cf6997ca4a39280ed8a031fc2d22621245f44bf
brotSchimmelt <tknebler+git@googlemail.com>
2024-03-12 23:34:41 +0100

    add get_time function

---
+++ utils/general.py
@@ -0,0 +1,31 @@
+from functools import wraps
+from time import perf_counter
+from typing import Any, Callable
+
+
+def get_time(func: Callable) -> Callable:
+    """Decorates a function to print its execution time.
+
+    Args:
+        func (Callable): The function to be measured and wrapped.
+
+    Returns:
+        Callable: A wrapper function that, when called, will execute the original `func`,
+        measure its execution time, print the duration, and then return the result of `func`.
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        start_time: float = perf_counter()
+        result: Any = func(*args, **kwargs)
+        end_time: float = perf_counter()
+
+        elapsed_time = end_time - start_time
+        minutes, seconds = divmod(elapsed_time, 60)
+
+        print(
+            f'"{func.__name__}()" took {int(minutes)} minutes and {seconds:.2f} seconds to execute'
+        )
+        return result
+
+    return wrapper
