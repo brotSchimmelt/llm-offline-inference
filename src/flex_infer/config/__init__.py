1c9812a55e02945f04b428e00717e2b196b98b80
brotSchimmelt <tknebler+git@googlemail.com>
2024-03-18 23:44:44 +0100

    add new settings

--- src/flex_infer/config/__init__.py
+++ src/flex_infer/config/__init__.py
@@ -1,2 +1,2 @@
 from .prompt_templates import PROMPT_TEMPLATES
-from .settings import DIRECTORIES, LOGGING, PATHS
+from .settings import LOGGING, RANDOM_SEED, SUPPORTED_QUANTIZATION_MODES
--- src/flex_infer/config/settings.py
+++ src/flex_infer/config/settings.py
@@ -1,3 +1,7 @@
 LOGGING = {
     "level": "INFO",
 }
+
+RANDOM_SEED = 42
+
+SUPPORTED_QUANTIZATION_MODES = ["awq", "gptq"]
