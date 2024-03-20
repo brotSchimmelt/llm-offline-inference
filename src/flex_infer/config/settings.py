d29247c71b72035dffaddd69b6529e3459497954
brotSchimmelt <tknebler+git@googlemail.com>
2024-03-20 13:48:50 +0100

    add model settings for testing

--- src/flex_infer/config/settings.py
+++ src/flex_infer/config/settings.py
@@ -6,3 +6,9 @@ LOGGING = {
 RANDOM_SEED = 42
 
 SUPPORTED_QUANTIZATION_MODES = ["awq", "gptq"]
+
+TEST_MODEL_SETTINGS = {
+    "model_name": "gemma-2b-instruct",
+    "model_path": "/bigwork/nhwpknet/models/gemma-2b-it",
+    "prompt_format": "llama2",
+}
